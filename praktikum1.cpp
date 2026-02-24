3f325ba44562616624ee18a907db38962c615720
Marchjul <marchellogiovani28@gmail.com>
2026-02-24 16:11:07 +0700

    pesan

---
+++ praktikum1.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    //mendeklarasikan variabel
+    int umur;
+    //memberi nilai dari variable
+    umur = 20;
+
+
+    //menampilkan isi
+    cout <<"Abra" << endl;
+    //menampilkan isi integer
+    cout << 20 <<endl;
+    //menampilkan isi variable
+    cout << umur <<endl;
+
+}
---
+++ rumusluass.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;   
+
+int main()
+{//start
+    float p,l,luas; //variablenya
+    //memasukan panjang
+    cout << "Masukan Panjang : "; 
+    cin >> p;
+    //memasukan lebar
+    cout << "Masukan Lebar : ";
+    cin >> l;
+    //hitung luas = pxl
+    luas = p*l;
+    //hasilnya
+    cout << "Hasilnya Adalah = " << luas;
+}//end
