b6b2a03334e72d4ad22a40306682f0544e38c87c
Marchjul <marchellogiovani28@gmail.com>
2026-02-24 16:03:11 +0700

    pesan

---
+++ jogja.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    bool cek = true;
+    char jk;
+    int angka = 10;
+    float tinggi = 175.0;
+    double harga = 900000.00;
+    string nama = "Pondoks";
+
+    jk = 'L' ;
+    cout << "Selamat Datang" <<endl;
+    cout << "Di TI UMY\n";
+    cout << "Yogyakarta\n";
+
+    nama = "Pondoks";
+    cout << "Nama Si Dia : " << nama ;
+
+}
